6e5cf46dca55e7ac65db3ed7e149d0cb6e015c99
Cole Peaden <cep42@students.uwf.edu>
2018-03-04 11:25:58 -0600

    Commit the factory method in progress for users.

---
+++ ScholarStation/UserFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScholarStation
+{
+    class UserFactory : IUserFactory
+    {
+        public User CreateUser(UserType type)
+        {
+            User user = null;
+            if (type == UserType.Standard)
+            {
+                user = new User("",1,UserType.Standard);
+            }
+            else if(type == UserType.Professor)
+            {
+                user = new User("", 2, UserType.Professor);
+            }
+            return user;
+        }
+    }
+}
