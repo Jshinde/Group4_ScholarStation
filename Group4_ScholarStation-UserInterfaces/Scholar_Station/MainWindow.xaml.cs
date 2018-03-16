f1681c682744ed83147f4de3a714281a53f31793
Goose1909 <cep42@students.uwf.edu>
2018-03-15 19:39:46 -0500

    Fixing issues

---
+++ Group4_ScholarStation-UserInterfaces/Scholar_Station/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using DataAccessControler;
+using System.Data;
+using System.Collections;
+using System;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Windows.Navigation;
+
+namespace Scholar_Station
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainWindowFrame.NavigationService.Navigate(new logInFrame());
+        }
+       
+    }
+}
