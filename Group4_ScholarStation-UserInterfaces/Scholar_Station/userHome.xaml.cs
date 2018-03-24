c6bb228717aecdfd9583dd48a5896cb8cede8abf
KennethDale1980 <kd31@students.uwf.edu>
2018-03-24 14:07:48 -0500

    Things keep changing...

---
+++ Group4_ScholarStation-UserInterfaces/Scholar_Station/userHome.xaml.cs
@@ -0,0 +1,46 @@
+﻿using ScholarStation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Scholar_Station
+{
+	/// <summary>
+	/// Interaction logic for userHome.xaml
+	/// </summary>
+	/// 
+
+	public partial class userHome : Page
+	{
+		private User user;
+
+		public userHome(User user)
+		{
+			this.user = user;
+			InitializeComponent();
+			welcomeMsg.Content = "Welcome, " + user.Email + "!";
+		}
+
+		private void createNew_Click(object sender, RoutedEventArgs e)
+		{
+			this.NavigationService.Navigate(new tutorPage(user));
+		}
+
+		private void joinExisting_Click(object sender, RoutedEventArgs e)
+		{
+			this.NavigationService.Navigate(new joinSessionFrame(user));
+		}
+
+	}
+}
